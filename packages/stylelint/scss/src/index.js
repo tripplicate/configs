3a8bcf72683b9fef9d2289767629e35d839887e0
Danil Sergeev <tripplicate.me@gmail.com>
2023-12-02 19:12:25 +0400

    chore(stylelint): release `stylelint-config-scss@1.0.2`

--- packages/stylelint/scss/src/index.js
+++ packages/stylelint/scss/src/index.js
@@ -3,4 +3,8 @@ module.exports = {
     'stylelint-config-standard-scss',
     '@trpl/stylelint-config-standard'
   ],
+  rules: {
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+  },
 };
