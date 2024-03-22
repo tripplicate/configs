da4df05df7fc71820467247ce2c641b41e851398
Danil Sergeev <tripplicate.me@gmail.com>
2024-03-23 00:45:10 +0400

    chore(stylelint): release `stylelint-config-standard@1.2.0`

--- packages/stylelint/standard/src/index.js
+++ packages/stylelint/standard/src/index.js
@@ -1,11 +1,10 @@
 module.exports = {
   extends: [
-    'stylelint-config-standard',
     'stylelint-config-clean-order',
     'stylelint-config-html'
   ],
   plugins: [
-    'stylelint-stylistic'
+    '@stylistic/stylelint-plugin'
   ],
   overrides: [
     {
@@ -61,95 +60,92 @@ module.exports = {
     // Declaration-block
     'declaration-block-single-line-max-declarations': null,
 
-    // Selector
-    'selector-max-empty-lines': 0,
-
     // Notation
     'font-weight-notation': ['named-where-possible'],
     'selector-class-pattern': null,
     'selector-nested-pattern': null,
     'selector-id-pattern': null,
 
-    // Stylistic
-    'stylistic/indentation': 2,
-    'stylistic/max-empty-lines': 1,
-    'stylistic/max-line-length': [120, {
+    // @stylistic
+    '@stylistic/indentation': 2,
+    '@stylistic/max-empty-lines': 1,
+    '@stylistic/max-line-length': [120, {
       ignore: ['comments'],
     }],
-    'stylistic/no-empty-first-line': true,
-    'stylistic/no-eol-whitespace': true,
-    'stylistic/no-extra-semicolons': true,
-    'stylistic/no-missing-end-of-source-newline': true,
-    'stylistic/unicode-bom': ['never'],
-    'stylistic/string-quotes': ['double'],
+    '@stylistic/no-empty-first-line': true,
+    '@stylistic/no-eol-whitespace': true,
+    '@stylistic/no-extra-semicolons': true,
+    '@stylistic/no-missing-end-of-source-newline': true,
+    '@stylistic/unicode-bom': ['never'],
+    '@stylistic/string-quotes': ['double'],
 
-    'stylistic/declaration-bang-space-before': ['always'],
-    'stylistic/declaration-bang-space-after': ['never'],
+    '@stylistic/declaration-bang-space-before': ['always'],
+    '@stylistic/declaration-bang-space-after': ['never'],
 
-    'stylistic/declaration-colon-newline-after': ['always-multi-line'],
-    'stylistic/declaration-colon-space-before': ['never'],
-    'stylistic/declaration-colon-space-after': ['always-single-line'],
+    '@stylistic/declaration-colon-newline-after': ['always-multi-line'],
+    '@stylistic/declaration-colon-space-before': ['never'],
+    '@stylistic/declaration-colon-space-after': ['always-single-line'],
 
-    'stylistic/declaration-block-semicolon-space-before': ['never'],
-    'stylistic/declaration-block-semicolon-space-after': ['always-single-line'],
-    'stylistic/declaration-block-semicolon-newline-before': ['never-multi-line'],
-    'stylistic/declaration-block-semicolon-newline-after': ['always-multi-line'],
-    'stylistic/declaration-block-trailing-semicolon': ['always'],
+    '@stylistic/declaration-block-semicolon-space-before': ['never'],
+    '@stylistic/declaration-block-semicolon-space-after': ['always-single-line'],
+    '@stylistic/declaration-block-semicolon-newline-before': ['never-multi-line'],
+    '@stylistic/declaration-block-semicolon-newline-after': ['always-multi-line'],
+    '@stylistic/declaration-block-trailing-semicolon': ['always'],
 
-    'stylistic/block-closing-brace-empty-line-before': ['never'],
-    'stylistic/block-closing-brace-newline-before': ['always-multi-line'],
-    'stylistic/block-closing-brace-newline-after': ['always', {
+    '@stylistic/block-closing-brace-empty-line-before': ['never'],
+    '@stylistic/block-closing-brace-newline-before': ['always-multi-line'],
+    '@stylistic/block-closing-brace-newline-after': ['always', {
       ignoreAtRules: ['if', 'else'],
     }],
-    'stylistic/block-closing-brace-space-before': ['always-single-line'],
-    'stylistic/block-closing-brace-space-after': null,
+    '@stylistic/block-closing-brace-space-before': ['always-single-line'],
+    '@stylistic/block-closing-brace-space-after': null,
 
-    'stylistic/block-opening-brace-newline-before': null,
-    'stylistic/block-opening-brace-newline-after': ['always-multi-line'],
-    'stylistic/block-opening-brace-space-before': ['always'],
-    'stylistic/block-opening-brace-space-after': null,
+    '@stylistic/block-opening-brace-newline-before': null,
+    '@stylistic/block-opening-brace-newline-after': ['always-multi-line'],
+    '@stylistic/block-opening-brace-space-before': ['always'],
+    '@stylistic/block-opening-brace-space-after': null,
 
-    'stylistic/selector-attribute-brackets-space-inside': ['never'],
-    'stylistic/selector-attribute-operator-space-after': ['never'],
-    'stylistic/selector-attribute-operator-space-before': ['never'],
+    '@stylistic/selector-attribute-brackets-space-inside': ['never'],
+    '@stylistic/selector-attribute-operator-space-after': ['never'],
+    '@stylistic/selector-attribute-operator-space-before': ['never'],
 
-    'stylistic/selector-combinator-space-before': ['always'],
-    'stylistic/selector-combinator-space-after': ['always'],
+    '@stylistic/selector-combinator-space-before': ['always'],
+    '@stylistic/selector-combinator-space-after': ['always'],
 
-    'stylistic/selector-descendant-combinator-no-non-space': true,
+    '@stylistic/selector-descendant-combinator-no-non-space': true,
 
-    'stylistic/selector-max-empty-lines': 0,
+    '@stylistic/selector-max-empty-lines': 0,
 
-    'stylistic/selector-pseudo-class-case': 'lower',
-    'stylistic/selector-pseudo-class-parentheses-space-inside': ['never'],
-    'stylistic/selector-pseudo-element-case': 'lower',
-    'stylistic/selector-list-comma-newline-before': ['never-multi-line'],
-    'stylistic/selector-list-comma-newline-after': ['always-multi-line'],
-    'stylistic/selector-list-comma-space-before': ['never'],
-    'stylistic/selector-list-comma-space-after': ['always-single-line'],
+    '@stylistic/selector-pseudo-class-case': 'lower',
+    '@stylistic/selector-pseudo-class-parentheses-space-inside': ['never'],
+    '@stylistic/selector-pseudo-element-case': 'lower',
+    '@stylistic/selector-list-comma-newline-before': ['never-multi-line'],
+    '@stylistic/selector-list-comma-newline-after': ['always-multi-line'],
+    '@stylistic/selector-list-comma-space-before': ['never'],
+    '@stylistic/selector-list-comma-space-after': ['always-single-line'],
 
-    'stylistic/media-feature-colon-space-before': ['never'],
-    'stylistic/media-feature-colon-space-after': ['always'],
-    'stylistic/media-feature-name-case': ['lower'],
-    'stylistic/media-feature-parentheses-space-inside': ['never'],
-    'stylistic/media-feature-range-operator-space-before': ['always'],
-    'stylistic/media-feature-range-operator-space-after': ['always'],
+    '@stylistic/media-feature-colon-space-before': ['never'],
+    '@stylistic/media-feature-colon-space-after': ['always'],
+    '@stylistic/media-feature-name-case': ['lower'],
+    '@stylistic/media-feature-parentheses-space-inside': ['never'],
+    '@stylistic/media-feature-range-operator-space-before': ['always'],
+    '@stylistic/media-feature-range-operator-space-after': ['always'],
 
-    'stylistic/media-query-list-comma-newline-before': ['never-multi-line'],
-    'stylistic/media-query-list-comma-newline-after': ['never-multi-line'],
-    'stylistic/media-query-list-comma-space-before': ['never'],
-    'stylistic/media-query-list-comma-space-after': ['always'],
+    '@stylistic/media-query-list-comma-newline-before': ['never-multi-line'],
+    '@stylistic/media-query-list-comma-newline-after': ['never-multi-line'],
+    '@stylistic/media-query-list-comma-space-before': ['never'],
+    '@stylistic/media-query-list-comma-space-after': ['always'],
 
-    'stylistic/at-rule-name-case': ['lower'],
-    'stylistic/at-rule-name-newline-after': ['always-multi-line'],
-    'stylistic/at-rule-name-space-after': ['always-single-line'],
+    '@stylistic/at-rule-name-case': ['lower'],
+    '@stylistic/at-rule-name-newline-after': ['always-multi-line'],
+    '@stylistic/at-rule-name-space-after': ['always-single-line'],
 
-    'stylistic/at-rule-semicolon-newline-after': ['always'],
-    'stylistic/at-rule-semicolon-space-before': ['never'],
+    '@stylistic/at-rule-semicolon-newline-after': ['always'],
+    '@stylistic/at-rule-semicolon-space-before': ['never'],
 
-    'stylistic/number-no-trailing-zeros': true,
-    'stylistic/number-leading-zero': ['always'],
+    '@stylistic/number-no-trailing-zeros': true,
+    '@stylistic/number-leading-zero': ['always'],
 
-    'stylistic/property-case': ['lower'],
+    '@stylistic/property-case': ['lower'],
   },
 };
